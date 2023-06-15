8428ec3db574fbb13d1b03033e1a3613579bcbf5
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-06-15 17:12:41 +0200

    Act

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,19 @@ import express from "express";
 import cors from "cors";
 import { configureRoutes } from "../routes/index";
 
+// Cargamos variables de entorno
+import dotenv from "dotenv";
+dotenv.config();
+
+const FRONT_END_URL: string = process.env.FRONT_END_URL as string;
+
 // Configuración del server
 export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:4000",
+    origin: FRONT_END_URL,
   })
 );
 
